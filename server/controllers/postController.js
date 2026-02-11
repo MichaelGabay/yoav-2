d715cccc76fd4d1eb18c23f47e57a9e38a48ba72
Micael Gabay <michaelgabay770@gmail.com>
2026-02-11 19:52:35 +0200

    1

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -61,12 +61,44 @@ const postCtrl = {
         }
     },
     // ?id=
-    async unlikePost(req, res) { },
+    async unlikePost(req, res) {
+        try {
+            const post = await postModel.findById(req.query.id)
+            if (!post) return res.status(404).json({ message: "post not found" })
+            if (!post.likes.includes(req.user._id)) return res.status(400).json({ message: "you can't unlike this post" })
+            post.likes = post.likes.filter((id) => id != req.user._id)
+            const resp = await post.save()
+            res.status(200).json({ message: "post unliked successfully", resp })
+        } catch (error) {
+            res.status(400).json({ message: "there was an error liking the post", error })
+        }
+    },
     // ->[]
-    async getPosts(req, res) { },
+    async getPosts(req, res) {
+        try {
+            const posts = await postModel.aggregate([{ $sample: { size: 10 } }])
+            res.status(200).json(posts)
+        } catch (error) {
+            res.status(400).json({ message: "there was an error getting posts", error })
+        }
+    },
     // ->[]
     async getPostsByUser(req, res) {
-
+        try {
+            const page = +req.query.page || 1
+            const limit = 5
+            const posts = await postModel.find().limit(limit).skip((page - 1) * limit);
+            const pages = Math.ceil(await postModel.countDocuments() / limit)
+            const resp = {
+                pages,
+                next: "http://localhost:3000/api/post/getUserPosts?page=" + (page + 1),
+                data: posts
+            }
+            if (page == pages) delete resp.next
+            res.status(200).json(resp)
+        } catch (error) {
+            res.status(400).json({ message: "there was an error getting posts", error })
+        }
     }
 }
 
